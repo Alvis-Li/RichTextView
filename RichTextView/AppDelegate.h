7277265133311e9747adafa08efe563bf1abe4d4
liqiancheng <lqcqc@sina.com>
2015-08-03 19:49:47 +0800

    Initial Commit

---
+++ RichTextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RichTextView
+//
+//  Created by levy on 15/8/3.
+//  Copyright (c) 2015年 levy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
